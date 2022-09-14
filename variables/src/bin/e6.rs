47160247ede1b768a9eac707ace622fd807fda05
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-09-14 16:59:52 -0300

    Variable shadowing.

---
+++ variables/src/bin/e6.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let mut x: i32 = 1;
+    x = 7;
+    // Shadowing and re-binding
+    let x = x; 
+
+
+    let y = 4;
+    // Shadowing
+    let y = "I can also be bound to text!"; 
+
+    println!("Success!");
+}
