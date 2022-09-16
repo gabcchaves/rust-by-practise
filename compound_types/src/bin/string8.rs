25fa0d9d1f90e1d87b2b47cb8a981098243c73a3
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-09-16 14:30:29 -0300

    String.

---
+++ compound_types/src/bin/string8.rs
@@ -0,0 +1,14 @@
+// Use two approaches to fix the error and without adding a new line
+fn main() {
+    let s = "hello, world".to_string();
+    let s1: String = s;
+
+    println!("Success!");
+}
+
+// fn main() {
+//     let s = &"hello, world".to_string();
+//     let s1: &str = s;
+//
+//     println!("Success!");
+// }
